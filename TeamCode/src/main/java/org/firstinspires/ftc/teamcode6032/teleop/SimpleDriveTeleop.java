e29e2f4ba61e455b57b673b09b0441ada8430f18
SciDev5 <jhuber2048@gmail.com>
2022-01-03 14:44:54 -0500

    add dpad movement to simple drive

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode6032/teleop/SimpleDriveTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode6032/teleop/SimpleDriveTeleop.java
@@ -31,8 +31,8 @@ public class SimpleDriveTeleop extends OpMode {
 
     @Override
     public void loop() {
-        final float fwd = gamepad1.left_stick_y;
-        final float strafe = gamepad1.left_stick_x;
+        final float fwd = gamepad1.left_stick_y+(gamepad1.dpad_up?1:0)+(gamepad1.dpad_down?-1:0);
+        final float strafe = gamepad1.left_stick_x+(gamepad1.dpad_right?1:0)+(gamepad1.dpad_left?-1:0);
         final float rot = gamepad1.right_stick_x;
         final boolean slow = gamepad1.left_bumper;
         final boolean fast = gamepad1.right_bumper;
