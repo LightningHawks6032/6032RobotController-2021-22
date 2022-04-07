b844068e9238599a6b5b17d97ab86857ca51cfeb
SciDev5 <jhuber2048@gmail.com>
2022-04-07 14:57:23 -0400

    make input test show that the data was updated

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode6032/teleop/InputTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode6032/teleop/InputTest.java
@@ -9,12 +9,16 @@ import org.firstinspires.ftc.teamcode6032.debug.dbuginput.DBugInput;
 @TeleOp(name = "InputTest", group = "test")
 public class InputTest extends LinearOpMode {
     @Override
-    public void runOpMode() {
+    public void runOpMode() throws InterruptedException {
         DBugData a = new DBugData("A",0);
         DBugData b = new DBugData("B",true);
         DBugData c = new DBugData("C",0.2,2);
         DBugInput i = new DBugInput(telemetry,gamepad1,a,b);
 
         i.run(this::isStopRequested);
+
+        telemetry.addLine("A="+a.getValueI()+";B="+b.getValueB()+";C="+c.getValueD());
+        telemetry.update();
+        Thread.sleep(5000);
     }
 }
